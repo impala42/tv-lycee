f767e0e162f765ecbe9a6f4b86658003491a0c00
Simon Chareyre <simonchareyre@mailo.com>
2026-04-19 17:58:34 +0200

    liste menu

--- admin/menus/index.php
+++ admin/menus/index.php
@@ -1,4 +1,30 @@
-<?php require '../utilisateurs/auth.php'; ?>
+<?php 
+require '../utilisateurs/auth.php'; 
+require '../../bdd/db.php';
+
+$date  = $_GET['date'] ?? '';
+$date = DateTimeImmutable::createFromFormat('Y-m-d', $date);
+
+// Si pas de date précisée on prend l'actuel
+if (!$date) {
+    header('Location: index.php?date='.date("Y-m-d"));
+    exit;
+}
+
+// Chercher les menus
+$stmt = $pdo->prepare("
+    SELECT m.id, e.nom AS entree, p.nom AS plat, l.nom AS laitage, d.nom AS dessert FROM Menu AS m 
+    JOIN Plat AS e ON m.id_entree = e.id 
+    JOIN Plat AS p ON m.id_plat_principal = p.id
+    JOIN Plat AS l ON m.id_laitage = l.id
+    JOIN Plat AS d ON m.id_dessert = d.id
+    WHERE m.jour = ? "
+);
+$stmt->execute([$date->format("Y-m-d")]);
+$menu = $stmt->fetch();
+
+
+?>
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -8,6 +34,23 @@
     <title>Admin - Menus</title>
 </head>
 <body>
+    <h1>Menus</h1>
     <a href="creer_menu.php">Nouveau Menu</a>
+    <h2>
+        <a href="index.php?date=<?= $date->modify("-1 day")->format("Y-m-d") ?>"> < </a> 
+         Menu du <?= $date->format("d/m") ?> 
+        <a href="index.php?date=<?= $date->modify("+1 day")->format("Y-m-d") ?>"> > </a>
+    </h2>
+    
+    <?php if (!$menu) : ?>
+        <p>Pas de menu défini pour ce jour.</p>
+    <?php else : ?>
+        <ul>
+            <li> <?= htmlspecialchars($menu['entree']) ?> </li>
+            <li> <?= htmlspecialchars($menu['plat']) ?> </li>
+            <li> <?= htmlspecialchars($menu['laitage']) ?> </li>
+            <li> <?= htmlspecialchars($menu['dessert']) ?> </li>
+        </ul>
+    <?php endif ?>
 </body>
 </html>
